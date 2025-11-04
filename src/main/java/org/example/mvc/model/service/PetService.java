34bfb8c6f966491425039c9a1c5b0215b7baf078
PARK HYEONG MIN <qd0420@gmail.com>
2025-11-04 11:49:00 +0900

    step03 - 추상화

--- src/main/java/org/example/mvc/model/service/PetService.java
+++ src/main/java/org/example/mvc/model/service/PetService.java
@@ -1,5 +1,10 @@
-package org.example.mvc.model.service;
+package org.example.mvc.service;
 
+import org.example.mvc.model.entity.Pet;
+
+import java.util.List;
 
 public interface PetService {
+    List<Pet> readAll();
+    void create(String name, int age, String category);
 }
